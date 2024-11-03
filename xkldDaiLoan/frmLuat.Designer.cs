74d87b89fb02aaa4f8652bf9996af9ba9148a51d
HungCuong <hungcuonngb48@gmail.com>
2024-11-03 21:38:21 +0700

    chức năng frm Luật

--- xkldDaiLoan/frmLuat.Designer.cs
+++ xkldDaiLoan/frmLuat.Designer.cs
@@ -28,21 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLuat));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.dgv_luat = new System.Windows.Forms.DataGridView();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnLamMoi = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNoidung = new System.Windows.Forms.TextBox();
+            this.txtMaLuat = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_luat)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.dgv_luat);
             this.panel1.Location = new System.Drawing.Point(4, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(546, 606);
             this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(544, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(552, 604);
+            this.panel2.TabIndex = 2;
             // 
             // dgv_luat
             // 
@@ -55,17 +78,140 @@
             this.dgv_luat.TabIndex = 0;
             this.dgv_luat.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_luat_CellContentClick);
             // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(392, 68);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(109, 23);
+            this.btnTimKiem.TabIndex = 1;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Controls.Add(this.btnLamMoi);
+            this.panel3.Controls.Add(this.btnXoa);
+            this.panel3.Controls.Add(this.btnSua);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.txtNoidung);
+            this.panel3.Controls.Add(this.txtMaLuat);
+            this.panel3.Controls.Add(this.btnThem);
+            this.panel3.Controls.Add(this.txtTimKiem);
+            this.panel3.Controls.Add(this.btnTimKiem);
+            this.panel3.Location = new System.Drawing.Point(548, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(552, 606);
+            this.panel3.TabIndex = 1;
+            // 
+            // btnLamMoi
+            // 
+            this.btnLamMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLamMoi.Location = new System.Drawing.Point(322, 425);
+            this.btnLamMoi.Name = "btnLamMoi";
+            this.btnLamMoi.Size = new System.Drawing.Size(103, 49);
+            this.btnLamMoi.TabIndex = 10;
+            this.btnLamMoi.Text = "Làm mới";
+            this.btnLamMoi.UseVisualStyleBackColor = true;
+            this.btnLamMoi.Click += new System.EventHandler(this.btnLamMoi_Click_1);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(140, 425);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(103, 49);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(322, 345);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(103, 49);
+            this.btnSua.TabIndex = 8;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nội dung";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mã luật";
+            // 
+            // txtNoidung
+            // 
+            this.txtNoidung.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNoidung.Location = new System.Drawing.Point(170, 254);
+            this.txtNoidung.Name = "txtNoidung";
+            this.txtNoidung.Size = new System.Drawing.Size(312, 34);
+            this.txtNoidung.TabIndex = 5;
+            // 
+            // txtMaLuat
+            // 
+            this.txtMaLuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaLuat.Location = new System.Drawing.Point(170, 191);
+            this.txtMaLuat.Name = "txtMaLuat";
+            this.txtMaLuat.Size = new System.Drawing.Size(312, 34);
+            this.txtMaLuat.TabIndex = 4;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(140, 345);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(103, 49);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiem.Location = new System.Drawing.Point(68, 68);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(251, 22);
+            this.txtTimKiem.TabIndex = 2;
+            // 
             // frmLuat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1097, 605);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Name = "frmLuat";
             this.Text = "frmLuat";
             this.Load += new System.EventHandler(this.frmLuat_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_luat)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -74,5 +220,17 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridView dgv_luat;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtMaLuat;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNoidung;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnLamMoi;
     }
 }
--- xkldDaiLoan/frmLuat.cs
+++ xkldDaiLoan/frmLuat.cs
@@ -28,17 +28,115 @@ namespace xkldDaiLoan
 
         private void dgv_luat_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            int r = e.RowIndex;
+            if (r >= 0)
+            {
+                txtMaLuat.Enabled = false;
+                btnThem.Enabled = false;
+                btnSua.Enabled = true;
+                btnXoa.Enabled = true;
+                txtMaLuat.Text = dgv_luat.Rows[r].Cells["maluat"].Value.ToString();
+                txtNoidung.Text = dgv_luat.Rows[r].Cells["noidung"].Value.ToString();
+            }
         }
 
         private void frmLuat_Load(object sender, EventArgs e)
         {
             getData();
 
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+
             dgv_luat.Columns[0].HeaderText = "Mã luật";
             dgv_luat.Columns[0].Width = 75;
             dgv_luat.Columns[1].HeaderText = "Nội dung";
             dgv_luat.Columns[1].Width = 225;
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            clearText();
+            getData();
+        }
+
+        public void clearText()
+        {
+            txtMaLuat.Enabled = true;
+            btnThem.Enabled = true;
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            txtMaLuat.Text = "";
+            txtNoidung.Text = "";
+            txtTimKiem.Text = "";
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string query = string.Format(
+                "insert into tb_luat VALUES(N'{0}', '{1}')",
+                txtMaLuat.Text,
+                txtNoidung.Text
+            );
+            if (kn.ThucThi(query) == true)
+            {
+                MessageBox.Show("Thêm mới thành công!");
+                btnLamMoi.PerformClick();
+            }
+            else
+            {
+                MessageBox.Show("Thêm mới thất bại!");
+            }
+        }
+
+        private void btnLamMoi_Click_1(object sender, EventArgs e)
+        {
+            clearText();
+            getData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string query = string.Format(
+               "update tb_luat set noidung = '{1}'where maluat = '{0}'",
+               txtMaLuat.Text,
+                txtNoidung.Text
+           );
+            if (kn.ThucThi(query) == true)
+            {
+                MessageBox.Show("Sửa thành công!");
+                btnLamMoi.PerformClick();
+            }
+            else
+            {
+                MessageBox.Show("Sửa thất bại!");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string query = string.Format(
+                "delete from tb_luat where maluat = '{0}'",
+                txtMaLuat.Text
+            );
+            if (kn.ThucThi(query) == true)
+            {
+                MessageBox.Show("Xóa thành công!");
+                btnLamMoi.PerformClick();
+            }
+            else
+            {
+                MessageBox.Show("Xóa thất bại!");
+            }
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string query = string.Format(
+                "select tb_luat.* from tb_luat where maluat like N'%{0}%'",
+                txtTimKiem.Text
+            );
+            DataSet ds = kn.LayDuLieu(query);
+            dgv_luat.DataSource = ds.Tables[0];
         }
     }
 }
